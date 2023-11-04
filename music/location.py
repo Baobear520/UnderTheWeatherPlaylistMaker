72bbedda5d47383fa83b4aa66658507a290b548e
Dima <aldmikon@gmail.com>
2023-11-04 11:41:48 +0800

    35. - Setting up exception handeling in location.py and weather.py code

--- music/location.py
+++ music/location.py
@@ -1,15 +1,18 @@
+import logging
 import geocoder
-import os, logging
-from .credentials import ow_credentials
 
 logger = logging.getLogger(__name__)
 
 
 def my_IP_location():
-
-    my_loc = geocoder.ip('me')
-    logger.info('Obtained {my_loc} object')
-    lat,lon = my_loc.latlng
-    logger.info('Lat is {lat}, lon is {lon}')
-    return lat,lon
+    #Obtaining geolocation coordinates
+    try: 
+        my_loc = geocoder.ip('me')
+        lat,lon = my_loc.latlng
+        logger.info('Obtained lat and lon ')
+        return lat,lon
+    except Exception as e:
+        logger.error(f"Coudn't obtain user's geolocation coordinates: {e}")
+        return None,None
+    
 
--- music/weather.py
+++ music/weather.py
@@ -1,34 +1,54 @@
 import logging
+from pyowm.commons import exceptions as ow_exceptions
 from .credentials import ow_credentials
 from .location import my_IP_location
 
 logger = logging.getLogger(__name__)
 
-def weather_type(mng,lat,lon):
-    
-    observation = mng.weather_at_coords(lat=lat, lon=lon)
-    # Grabbing current weather status
-    weather = observation.weather.status
-    #Define lists of similar weather types
-    rainy = ['Thunderstorm','Drizzle','Rain']
-    cloudy = ['Clouds']
-    sunny = ['Clear']
-    snowy_or_athmosphere = ['Snow','Atmosphere']
-    if weather in rainy:
-        return 'Rainy', weather
-    elif weather in cloudy:
-        return 'Cloudy', weather
-    elif weather in sunny:
-        return 'Sunny', weather
-    else:
-        return 'Snowy/Athmosphere', observation.weather.detailed_status
-        
+
+def weather_type(mng, lat, lon):
+    try:
+        observation = mng.weather_at_coords(lat=lat, lon=lon)
+        weather = observation.weather.status
+        detailed_weather = observation.weather.detailed_status
+
+        #Creating a dictionary of 'condition': conditions_list  like objects
+        weather_conditions = {
+            'Rainy': ['Thunderstorm', 'Drizzle', 'Rain'],
+            'Cloudy': ['Clouds'],
+            'Sunny': ['Clear'],
+            'Snowy/Athmosphere': ['Snow', 'Atmosphere']
+        }
+
+        for condition, conditions_list in weather_conditions.items():
+            if weather in conditions_list:
+                if condition == 'Snowy/Athmosphere':
+                    return condition,  detailed_weather
+                return condition, weather
+
+        # If none of the predefined conditions matched
+        return 'Unknown', weather
+
+    except ow_exceptions.ParseAPIResponseError as e:
+        logger.error(f"Couldn't obtain weather status (weather: {weather}): {e}")
+        return None, None
+    except Exception as e:
+        logger.error(f'An unexpected error occurred: {e}')
+        return None, None
+
 
 def city_ID(mng,lat,lon):
-    
     # Search for the nearest city based on coordinates
-    observation = mng.weather_at_coords(lat, lon)
-    location = observation.location
-    city_id = location.id
-    return city_id
+    try:
+        observation = mng.weather_at_coords(lat, lon)
+        location = observation.location
+        city_id = location.id
+        logger.info(f' Obtained city_id value: {city_id}')
+        return city_id
+    except ow_exceptions.ParseAPIResponseError as e:
+        logger.error(f"Couldn't obtain city_id: {e}")
+        return None
+    except Exception as e:
+        logger.error(f'An unexpected error occured: {e}')
+        return None
        
