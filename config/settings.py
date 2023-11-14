3980cf768b999d82d9b86c08af54eb4f352f6d11
Dima <aldmikon@gmail.com>
2023-11-14 18:56:11 +0800

    47.  - Creating static folder in music app  - Creating static/css/styles.css, static/images/two_phones.png

--- config/settings.py
+++ config/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'music/static/' 
+STATIC_URL = '/static/' 
 
-
-
+STSTIC_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from music import urls as music_urls
+from . import settings
 
 urlpatterns = [
     path('', include(music_urls),name='home'),
     path('admin/', admin.site.urls),
-]
+    ]
+
