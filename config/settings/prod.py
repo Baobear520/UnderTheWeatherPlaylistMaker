81a4dd1976112497c2a864a260c9a6e970857f48
Dima <aldmikon@gmail.com>
2023-12-05 19:07:35 +0800

    66. Adding ALLOWED_HOSTS

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = 'https://orca-app-qtor6.ondigitalocean.app/undertheweatherplaylistmaker2'
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
