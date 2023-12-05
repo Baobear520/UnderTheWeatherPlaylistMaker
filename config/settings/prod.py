b2233c1418d71451f3c22064253a62c8ee3847a1
Dima <aldmikon@gmail.com>
2023-12-05 19:32:07 +0800

    68. Running collectstatic Fixing bug in ALLOWED_HOSTS

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = 'https://orca-app-qtor6.ondigitalocean.app/'
+ALLOWED_HOSTS = ['https://orca-app-qtor6.ondigitalocean.app/']
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
