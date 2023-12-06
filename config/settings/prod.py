35562ec1515ccb7c5a57872fad0ac6ec4056db7e
Dima <aldmikon@gmail.com>
2023-12-06 08:20:57 +0800

    69 Changing ALLOWED_HOSTS value

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,10 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://orca-app-qtor6.ondigitalocean.app/']
+ALLOWED_HOSTS = [
+    'orca-app-qtor6.ondigitalocean.app',
+    'orca-app-qtor6.ondigitalocean.app/undertheweatherplaylistmaker2',
+    'localhost']
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
