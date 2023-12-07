1b914731094bff18e2fa0a24557227e8f7518c1a
Dima <aldmikon@gmail.com>
2023-12-07 15:50:06 +0800

    83. Reconfiguring urlconf

--- music/urls.py
+++ music/urls.py
@@ -5,8 +5,8 @@ from . import views
 logger = logging.getLogger(__name__)
 
 urlpatterns = [
-    path('', view=views.home_page,name='home'),
-    path('login',view=views.test_login, name='login'),
+    path('',view=views.login, name='login'),
+    path('authenticate',view=views.authenticate, name='authenticate'),
     path('contacts',view=views.contacts,name='contacts'),
     path('about',view=views.about,name='about'),
     path('create-playlist/',view=views.create_playlist,name='create-playlist'),
