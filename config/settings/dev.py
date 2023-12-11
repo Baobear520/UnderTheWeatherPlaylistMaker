fefc402210ee60d17642fe8ff76bf3af2da3c923
Dima <aldmikon@gmail.com>
2023-12-11 15:03:29 +0800

    90. Modifying ALLOWED_HOSTS in settings/dev.py (can be run on both the local machine and the server)

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -3,6 +3,6 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
 SECRET_KEY = '_!acfcjalq49#s#y4o*92se1mv=yumpsmzcdnbd^sx$l_s8ko6'
