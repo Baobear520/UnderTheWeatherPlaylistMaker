d75126fb5114006a9e552eddefb621a9f7dfd888
Dima <aldmikon@gmail.com>
2023-12-13 15:19:25 +0800

    100. Changing the url for login

--- music/urls.py
+++ music/urls.py
@@ -5,12 +5,11 @@ from . import views
 logger = logging.getLogger(__name__)
 
 urlpatterns = [
-    path('login',view=views.login, name='login'),
+    path('',view=views.login, name='login'),
     path('login/success',view=views.login_success,name='login-success'),
     path('authenticate',view=views.authenticate, name='authenticate'),
+    path('create-playlist/',view=views.create_playlist,name='create-playlist'),
+    path('create-playlist/success',view=views.created,name='created'), 
     path('contacts',view=views.contacts,name='contacts'),
     path('about',view=views.about,name='about'),
-    path('create-playlist/',view=views.create_playlist,name='create-playlist'),
-    path('create-playlist/success',view=views.created,name='created') 
-    
 ]
