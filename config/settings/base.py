fa607814f70385a145e002bec6b07791095e417e
Dima <aldmikon@gmail.com>
2024-02-09 11:48:54 +0800

    102th commit Removing unused libraries Renaming playgraound.py -> delete_playlists.py Deleting requirements.txt

--- config/settings/base.py
+++ config/settings/base.py
@@ -58,6 +58,8 @@ DATABASES = {
     }
 }
 
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- music/scripts/delete_playlists.py
+++ music/scripts/delete_playlists.py
@@ -1,18 +1,11 @@
-import os, geocoder
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from pyowm.owm import OWM
+
  
 
-def my_func():
-    auth_manager = SpotifyOAuth(
-            scope='user-library-read user-top-read playlist-modify-public')
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-    top_artists = sp.current_user_top_artists(limit=20,time_range='long_term')
-    print(f"Obtained a list of user's top-20 artists")
-    print(top_artists)
-
 def delete_all_playlists():
+    """Deleting all the users playlists"""
+
     auth_manager = SpotifyOAuth(
             scope='user-library-read user-top-read playlist-modify-public')
     sp = spotipy.Spotify(auth_manager=auth_manager)
@@ -24,6 +17,7 @@ def delete_all_playlists():
             print(f"Playlist {playlist['name']} has been deleted")
     except Exception as e:
             print('No playlists found')
+
 
 if __name__ == '__main__':
      delete_all_playlists()
