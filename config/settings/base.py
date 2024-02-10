199bf96e20e1100ac7e072acdfe7961d4cb2a64a
Dima <aldmikon@gmail.com>
2024-02-10 14:44:46 +0800

    104th - Installing django-redis - Configuring redis as a cache backend

--- config/settings/base.py
+++ config/settings/base.py
@@ -58,8 +58,6 @@ DATABASES = {
     }
 }
 
-
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -6,3 +6,13 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
 SECRET_KEY = '_!acfcjalq49#s#y4o*92se1mv=yumpsmzcdnbd^sx$l_s8ko6'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+} 
